6350276f4567a4cae5bdf80bccb544230a86ba0e
jessica <jessicahuangtfg@gmail.com>
2019-01-06 23:09:51 +0800

    Fidth

--- AddClass.php
+++ AddClass.php
@@ -16,36 +16,12 @@
     if (!$db_selected) 
         die ('Can\'t use: ' . mysqli_error());
             
-        //&&isset($_POST['time'])&&isset($_POST['room'])
-
-
-            /*$ndate = $_POST['date'];
-            $ntime = $_POST['time'];
-            $nroom = $_POST['room']; 
-            $nuser = $_POST['user'];*/
             $ndate = $_POST['date'];
             $ntime = $_POST['time'];
             $nroom = $_POST['room']; 
             $nuser = $_POST['user'];
             
-            /*$stmt = $conn->prepare("INSERT INTO class (user,room,time,date) VALUES (?,?,?,?)" );
 
-            if ($stmt === FALSE) {
-                die ("Mysql Error: " . $conn->error);
-            }
-
-            $stmt->bind_param("sssi",$date,$time,$room,$user);
-            
-            try{
-                echo "successfully".$_REQUEST['date'];
-                $date=$ndate;
-                $time=$ntime;
-                $room=$nroom;
-                $user= 1 ;
-            }
-            catch(Exception $e) {
-                echo 'Message: ' .$e->getMessage();
-            }*/
         if(isset($_POST['tabletype']) && $_POST['tabletype']== 'semester'){
             $nweek = $_POST['week'];
             $sql = "INSERT INTO semester (week, time, room ,user) VALUES ('" .$nweek."','".$ntime."','".$nroom."','".$_SESSION[$nuser]."')";   
@@ -61,13 +37,8 @@
          } else {
              echo "Error: " . $sql . "<br>" . $conn->error;
          } 
-        //echo "successfully".$_REQUEST['date'];
-       // $stmt->close();
-        $conn->close(); 
 
-      // print_r( $_REQUEST);
-   
-   
+        $conn->close(); 
 
 
 ?>
